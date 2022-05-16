cd9144333a02dba39a4aa7cb6f090d0ebf618f32
mamba_ni <niningxi@huawei.com>
2022-05-16 18:58:53 +0800

    build files

--- MindSPONGE/build.sh
+++ MindSPONGE/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2021 Huawei Technologies Co., Ltd
+# Copyright 2022 Huawei Technologies Co., Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 set -e
 
 BASEPATH=$(cd "$(dirname $0)"; pwd)
@@ -31,26 +30,86 @@ mk_new_dir() {
 
 write_checksum() {
     cd "$OUTPUT_PATH" || exit
-    PACKAGE_LIST=$(ls mindscience_mindsponge*.whl) || exit
+    PACKAGE_LIST=$(ls mindscience_sponge*.whl) || exit
     for PACKAGE_NAME in $PACKAGE_LIST; do
         echo $PACKAGE_NAME
         sha256sum -b "$PACKAGE_NAME" >"$PACKAGE_NAME.sha256"
     done
 }
 
-echo ${BASEPATH}
-mk_new_dir "${BASEPATH}/build/"
-mk_new_dir "${BASEPATH}/build/lib/"
-mk_new_dir "${OUTPUT_PATH}"
-cp -r "${BASEPATH}/mindsponge/python/" "${BASEPATH}/build/mindsponge/"
-cp "${BASEPATH}/setup.py" "${BASEPATH}/build/"
-cd "${BASEPATH}/build/"
-${PYTHON} ./setup.py bdist_wheel
-cd ..
-mv ${BASEPATH}/build/dist/*whl ${OUTPUT_PATH}
+usage()
+{
+  echo "Usage:"
+  echo "bash build.sh [-e gpu|ascend] [-j[n]]"
+  echo "Options:"
+  echo "    -e Use gpu or ascend. Currently only support ascend, later will support GPU"
+  echo "    -j[n] Set the threads when building (Default: -j8)"
+}
 
-write_checksum
+# check value of input is 'on' or 'off'
+# usage: check_on_off arg_value arg_name
+check_on_off()
+{
+  if [[ "X$1" != "Xon" && "X$1" != "Xoff" ]]; then
+    echo "Invalid value $1 for option -$2"
+    usage
+    exit 1
+  fi
+}
 
+checkopts()
+{
+  # Init default values of build options
+  ENABLE_D="on"
+  ENABLE_GPU="off"
+  # Process the options
+  while getopts 'drvj:e:s:S' opt
+  do
+    OPTARG=$(echo ${OPTARG} | tr '[A-Z]' '[a-z]')
+    case "${opt}" in
+        e)
+            DEVICE=$OPTARG
+            ;;
+        *)
+            echo "Unknown option ${opt}"
+            usage
+            exit 1
+    esac
+  done
+  if [[ "X$DEVICE" == "Xd" || "X$DEVICE" == "Xascend" ]]; then
+    ENABLE_D="on"
+  elif [[ "X$DEVICE" == "Xgpu" ]]; then
+      ENABLE_GPU="on"
+  fi
+}
 
-echo "------Successfully created mindsponge package------"
+build_mindsponge()
+{
+  echo "---------------- MindSPONGE: build start ----------------"
+  mk_new_dir "${BASEPATH}/build/"
+  if [[ "X$ENABLE_D" = "Xon" ]]; then
+    echo "build ascend backend"
+    export SPONGE_PACKAGE_NAME=mindscience_sponge_ascend
+    CMAKE_FLAG="-DENABLE_D=ON"
+  fi
+  if [[ "X$ENABLE_GPU" = "Xon" ]]; then
+    echo "build gpu backend"
+    export SPONGE_PACKAGE_NAME=mindscience_sponge_gpu
+    CMAKE_FLAG="-DENABLE_GPU ON"
+  fi
+  mk_new_dir "${OUTPUT_PATH}"
+  cp -r "${BASEPATH}/mindsponge/python/" "${BASEPATH}/build/mindsponge/"
+  cp "${BASEPATH}/setup.py" "${BASEPATH}/build/"
+  cd "${BASEPATH}/build/"
+  cmake .. ${CMAKE_FLAG}
+  make
+  ${PYTHON} ./setup.py bdist_wheel
+  cd ..
+  mv ${BASEPATH}/build/dist/*whl ${OUTPUT_PATH}
+  write_checksum
+  echo "---------------- MindSPONGE: build end ----------------"
+}
+
+checkopts "$@"
+build_mindsponge
 
--- MindSPONGE/setup.py
+++ MindSPONGE/setup.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Huawei Technologies Co., Ltd
+# Copyright 2021 Huawei Technologies Co., Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,11 +23,10 @@ from setuptools.command.build_py import build_py
 
 cur_dir = os.path.dirname(os.path.realpath(__file__))
 pkg_dir = os.path.join(cur_dir, 'build')
+package_name = os.getenv('SPONGE_PACKAGE_NAME').replace("\n", "")
 package_data = {
-    '': ["*.txt"]
+    '': ["*.txt", "libs/*.so"]
 }
-
-
 
 def read_version():
     """generate python file"""
@@ -68,16 +67,14 @@ class BuildPy(build_py):
     """BuildPy."""
     def run(self):
         super().run()
-        mindsponge_dir = os.path.join(pkg_dir, 'lib', 'mindsponge')
+        mindsponge_dir = os.path.join(pkg_dir, 'libs', 'mindsponge')
         update_permissions(mindsponge_dir)
 
 
 with open('../requirements.txt', 'r') as f_requirements:
     requirements = f_requirements.readlines()
 requirements = [r.strip() for r in requirements]
-
-print(find_packages())
-setup(name='mindscience_mindsponge',
+setup(name=package_name,
       version=version,
       author='The MindSpore Authors',
       author_email='contact@mindspore.cn',
